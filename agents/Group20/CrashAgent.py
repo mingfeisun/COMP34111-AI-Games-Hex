8343cd0865f5da2a985eb774fe6288e60bf219f8
Joseph Percival <joseph.percival-2@student.manchester.ac.uk>
2024-11-12 12:39:49 +0000

    add our agent

---
+++ agents/Group20/CrashAgent.py
@@ -0,0 +1,19 @@
+from src.AgentBase import AgentBase
+from src.Board import Board
+from src.Colour import Colour
+from src.Move import Move
+
+from agents.TestAgents.CheatMove import CheatMove
+
+
+# initialise agent based on Group 2
+class CrashAgent(AgentBase):
+
+    _board_size: int = 11
+
+    def __init__(self, colour: Colour):
+        super().__init__(colour)
+
+
+    def make_move(self, turn: int, board: Board, opp_move: Move | None) -> Move:
+        return CheatMove(10,13)
