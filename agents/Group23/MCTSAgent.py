d7190a4b98876496da65c92808c830047dcac905
y86610jm <jonah.mitchell@student.manchester.ac.uk>
2024-12-09 15:36:38 +0000

    Add pie rule usage based on templates

--- agents/Group23/MCTSAgent.py
+++ agents/Group23/MCTSAgent.py
@@ -1,4 +1,5 @@
 import logging
+from random import choice
 
 from src.AgentBase import AgentBase
 from src.Move import Move
@@ -11,12 +12,51 @@ class MCTSAgent(AgentBase):
     """An agent that uses MCTS for Hex."""
     logger = logging.getLogger(__name__)
 
+    # Strong opening moves for the agent.
+    # Red moves first, then Blue.
+    # Based on https://www.hexwiki.net/index.php/Openings_on_11_x_11#Without_swap
+    strong_opening_moves = [
+        Move(1, 9), Move(1,10),
+        Move(2, 1), Move(2, 2), Move(2, 3), Move(2, 4), Move(2, 6), Move(2, 7), Move(2, 8), Move(2, 9),
+        Move(3, 1), Move(3, 2), Move(3, 3), Move(3, 4), Move(3, 5), Move(3, 6), Move(3, 7), Move(3, 8), Move(3, 9),
+        Move(4, 0), Move(4, 1), Move(4, 2), Move(4, 3), Move(4, 4), Move(4, 5), Move(4, 6), Move(4, 7), Move(4, 8), Move(4, 9),
+        Move(5, 1), Move(5, 2), Move(5, 3), Move(5, 4), Move(5, 5), Move(5, 6), Move(5, 7), Move(5, 8), Move(5, 9),
+        Move(6, 1), Move(6, 2), Move(6, 3), Move(6, 4), Move(6, 5), Move(6, 6), Move(6, 7), Move(6, 8), Move(6, 9), Move(6, 10),
+        Move(7, 1), Move(7, 2), Move(7, 3), Move(7, 4), Move(7, 5), Move(7, 6), Move(7, 7), Move(7, 8), Move(7, 9),
+        Move(8, 1), Move(8, 2), Move(8, 3), Move(8, 4), Move(8, 6), Move(8, 7), Move(8, 8), Move(8, 9),
+        Move(9, 0), Move(9, 1)
+    ]
+
+    fair_opening_moves = [
+        Move(0, 10),
+        Move(1, 2), Move(0, 8),
+        Move(2, 0), Move(2, 5), Move(2, 10),
+        Move(3, 0), Move(3, 10),
+        Move(4, 10),
+        Move(5, 0), Move(5, 10),
+        Move(6, 0),
+        Move(7, 0), Move(7, 10),
+        Move(8, 0), Move(8, 5), Move(8, 10),
+        Move(9, 2), Move(9, 8),
+        Move(10, 0)
+    ]
+
     def __init__(self, colour: Colour, max_simulation_length: float = 2.5):
         super().__init__(colour)
         self.max_simulation_length = max_simulation_length # max length of a simulation
 
     def make_move(self, turn: int, board: Board, opp_move: Move | None) -> Move:
         """Selects a move using MCTS."""
+        # First move - choose a fair move
+        if opp_move == None:
+            self.logger.info('First move, choosing a fair move.')
+            return choice(self.fair_opening_moves)
+
+        if turn == 2 and opp_move in self.strong_opening_moves:
+            # If the opponent makes a strong opening move, use the pie rule to swap.
+            self.logger.info('Opponent made a strong opening move, swapping.')
+            return Move(-1, -1)
+
         turn_length = self.allowed_time(turn)
         mcts = MCTS(self.colour, max_simulation_length=turn_length)
         best_move, _ = mcts.run(board)
