92143c208c3ea99081c1aa43417dcc6ee3f4aa27
j84846ky <kwan.yip@student.manchester.ac.uk>
2025-12-04 21:31:03 +0000

    Add current_turn attribute in MCTS class

--- agents/Group21/MCTSAgent.py
+++ agents/Group21/MCTSAgent.py
@@ -11,7 +11,8 @@ class MCTSAgent(AgentBase):
         self.mcts = MCTS(colour)
 
     def make_move(self, turn: int, board: Board, opp_move: Move | None) -> Move:
-        # Update MCTS with opponents move, select a move using MCTS and then update again
+        # Update MCTS with current turn number, opponents move, select a move using MCTS and then update again
+        self.mcts.current_turn = turn
         self.mcts.update(board, opp_move)
         move = self.mcts.run()
         self.mcts.update(board, move)
